a20204321d594cde9b8f5c437c017ad07d68cec1
Joe Zhou <joeflyingeagle@gmail.com>
2014-07-22 22:36:21 -0400

    First time all

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,9 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.phonegap.plugins.barcodescanner": "0.6.0"
+}
+// BOTTOM OF METADATA
+});
